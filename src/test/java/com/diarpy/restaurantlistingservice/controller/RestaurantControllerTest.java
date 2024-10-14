790304258ed5624c1df4b2abcc9c891c2e5f8161
Rutzno <d.rutzno@gmail.com>
2024-10-14 10:07:06 +0000

    add RestaurantControllerTest.java

---
+++ src/test/java/com/diarpy/restaurantlistingservice/controller/RestaurantControllerTest.java
@@ -0,0 +1,31 @@
+package com.diarpy.restaurantlistingservice.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Mack_TB
+ * @since 14/10/2024
+ * @version 1.0.0
+ */
+
+class RestaurantControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllRestaurants() {
+    }
+
+    @Test
+    void getRestaurantById() {
+    }
+
+    @Test
+    void saveRestaurant() {
+    }
+}
